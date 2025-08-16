c4e418dde69b5fc8654f7c1ae1d6ea0edcb0ddb0
Promode <pramoddutta_96@yahoo.com>
2025-08-16 10:08:57 +0530

    Locators

--- src/test/java/com/thetestingacademy/CommonToAll.java
+++ src/test/java/com/thetestingacademy/CommonToAll.java
@@ -1,17 +1,27 @@
 package com.thetestingacademy;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 
 public class CommonToAll {
 
-    public FirefoxDriver driver;
+    public ChromeDriver driver;
 
     public  void openBrowser(WebDriver driver, String url){
         driver.get(url);
+        driver.manage().window().maximize();
     }
 
+
     public void closeBrowser(WebDriver driver){
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         driver.quit();
     }
 
